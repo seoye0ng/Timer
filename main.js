0aedf5f30b12f6cb87f7b29ca96ad148dcc7402c
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-17 23:54:42 +0900

    Feat: 시간/분/초를 설정하는 기능 추가
    - 각 시간 박스를 클릭하면 숫자가 올라간다.
    - start버튼과 reset버튼의 색이 변한다.

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
 const startBtn = document.querySelector('.start-btn');
-const hTime = document.querySelector('.hrs');
+const resetBtn = document.querySelector('.reset-btn');
 
+// 1. 시간/분/초를 입력했을 때
+function setTime() {
+  const hrs = document.querySelector('.hrs');
+  const min = document.querySelector('.min');
+  const sec = document.querySelector('.sec');
+
+  const setHrs = (e) => {
+    startBtn.classList.add('active');
+    resetBtn.classList.add('active');
+
+    let hrsNum = parseInt(e.target.textContent);
+    if (hrsNum < 24) {
+      hrsNum += 1;
+      e.target.textContent = hrsNum;
+    } else {
+      e.target.textContent = '00';
+    }
+  };
+
+  const setMinSec = (e) => {
+    startBtn.classList.add('active');
+    resetBtn.classList.add('active');
+
+    let minSecNum = parseInt(e.target.textContent);
+    if (minSecNum < 60) {
+      minSecNum += 10;
+      e.target.textContent = minSecNum;
+    } else {
+      e.target.textContent = '00';
+    }
+  };
+  hrs.addEventListener('click', setHrs);
+  min.addEventListener('click', setMinSec);
+  sec.addEventListener('click', setMinSec);
+}
+
+// start 버튼을 눌렀을 때
 startBtn.addEventListener('click', (e) => {
   e.target.textContent = 'PAUSE';
 });
+
+setTime();
